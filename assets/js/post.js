d6b1753f04a4787bb8cbf0d933dda6ba4bd51d10
Weiqiang Yu <weiqiang.yu@shopee.com>
2022-01-09 14:29:25 +0800

    Add mark for toc

--- assets/js/post.js
+++ assets/js/post.js
@@ -12,6 +12,12 @@ $(document).ready(() => {
 
   // estimate read minutes
   readMinutes()
+
+  // scroll match the toc
+  $(window).bind('scroll', locateToc);
+
+  // add click event for all toc
+  clickToc()
 })
 
 function readMinutes() {
@@ -22,4 +28,46 @@ function readMinutes() {
   } else {
     $('#read-minutes').text('less than ' + 1 + ' min')
   }
+}
+
+function clickToc() {
+  // all anchors of toc
+  let toc_hs = $('.toc').find('li')
+  for (let i = 0; i < toc_hs.length; ++i) {
+    $(toc_hs[i]).click(() => {
+      // cancel scroll event for window
+      $(window).unbind('scroll')
+      $(toc_hs).removeClass('active')
+      $(toc_hs[i]).addClass('active')
+      // recover the scroll event for window
+      setTimeout(() => {
+        $(window).bind('scroll', locateToc);
+      }, 1000);
+    })
+  }
+}
+
+function locateToc() {
+  let headerHeight = document.getElementById('header').offsetHeight
+  let scroll_height = $(window).scrollTop() + headerHeight + 1
+  if (scrollToBottom(scroll_height)) {
+    // already scroll to bottom
+    return
+  }
+  // all anchors of this article
+  let post_hs = $('.article :header')
+  // all anchors of toc
+  let toc_hs = $('.toc').find('li')
+  $(toc_hs).removeClass('active')
+  for (let i = 0; i < post_hs.length; i++) {
+    let h_height = $(post_hs[i]).offset().top
+    if (h_height < scroll_height) {
+      $(toc_hs).removeClass('active')
+      $(toc_hs[i]).addClass('active')
+    }
+  }
+}
+
+function scrollToBottom(scroll_height) {
+  return scroll_height >= $('.article').height()
 }
